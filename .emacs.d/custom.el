6b36670605bd627d9ba283c2d4bcd167f27987af
peng-yo <2485166642@qq.com>
2023-03-20 18:03:07 +0800

    refactor the file of init.el, push it to the packages.el

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,12 +3,92 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application tramp :machine "localhost")
+      tramp-connection-local-darwin-ps-profile)
+     ((:application tramp :machine "pengchunyoudeMacBook-Air.local")
+      tramp-connection-local-darwin-ps-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
  '(custom-safe-themes
    '("1781e8bccbd8869472c09b744899ff4174d23e4f7517b8a6c721100288311fa5" default))
  '(org-agenda-files
    '("/Volumes/Samsung_T5/Document/RoamNotes/Tasks.org_archive"))
  '(package-selected-packages
-   '(bm forge org-roam dashboard markdown-preview-mode htmlize ox-reveal lsp-latex auctex jump-char vimish-fold realgud-lldb realgud beacon leaf good-scroll vscode-icon all-the-icons-completion hasklig-mode lsp-haskell haskell-mode clang-format+ expand-region multiple-cursors auto-yasnippet yasnippet-snippets dired-hide-dotfiles dired-open all-the-icons-dired dired-single eterm-256color lsp-mode visual-fill-column org-bullets ivy-prescient counsel all-the-icons lsp-ivy racket-mode wrap-region smartparens surround python-black py-autopep8 yapfify lsp-pyright yasnippet flycheck projectile vterm general lsp-java helm-z rainbow-delimiters magit smex paredit zenburn-theme zenscript-mode color-theme-sanityinc-tomorrow org evil-nerd-commenter lsp-ui company-box dap-mode helm-lsp company which-key helm exotica-theme use-package))
+   '(dumb-jump bm forge org-roam dashboard markdown-preview-mode htmlize ox-reveal lsp-latex auctex jump-char vimish-fold realgud-lldb realgud beacon leaf good-scroll vscode-icon all-the-icons-completion hasklig-mode lsp-haskell haskell-mode expand-region multiple-cursors auto-yasnippet yasnippet-snippets dired-hide-dotfiles dired-open all-the-icons-dired dired-single eterm-256color lsp-mode visual-fill-column org-bullets ivy-prescient counsel all-the-icons lsp-ivy racket-mode wrap-region smartparens surround python-black py-autopep8 yapfify lsp-pyright yasnippet flycheck projectile vterm general lsp-java rainbow-delimiters magit smex paredit zenburn-theme zenscript-mode color-theme-sanityinc-tomorrow org evil-nerd-commenter lsp-ui company-box dap-mode helm-lsp company which-key helm exotica-theme use-package))
  '(warning-suppress-types
    '((use-package)
      (use-package)
--- .emacs.d/kbd.el
+++ .emacs.d/kbd.el
@@ -6,9 +6,7 @@
  "C-c k" 'kill-this-buffer
  "C-c e" 'eval-buffer
  "C-c t" 'vterm-other-window
- "C-c s" 'switch-to-buffer
  "C-c <f5>" 'treemacs
- "C-c m" 'magit-commit
  "C-c M-r" 'sp-wrap-round
  "C-c M-c" 'sp-wrap-curly
  "C-c RET" 'yas-expand
@@ -16,6 +14,12 @@
  "C-c u" 'sp-unwrap-sexp
  "M-n" 'good-scroll-up
  "M-p" 'good-scroll-down
+ "C-c l i" 'lsp-ui-imenu
+ "M-g j" 'dumb-jump-hydra/body
+ "C-v" 'my-scroll-up
+ "M-v" 'my-scroll-down
+ "C-M-v" 'my-scroll-other-window-up
+ "C-S-M-v" 'my-scroll-other-window-down
  )
 
 (global-unset-key (kbd "<escape>"))
@@ -23,7 +27,7 @@
 (global-unset-key (kbd "C-SPC"))
 (general-define-key
  "C-c -" 'select-current-line
- "C-SPC SPC" 'er/expand-region
+ "C-=" 'er/expand-region
  "C-SPC b b" 'bm-toggle
  "C-SPC b n" 'bm-next
  "C-SPC b p" 'bm-previous
@@ -34,6 +38,7 @@
  "C-SPC f f" 'vimish-fold
  "C-SPC f d" 'vimish-fold-delete
  "M-m" 'jump-char-forward
+ "C-M-m" 'jump-char-backward
  "C-]" 'add-curly
  "C-c f" 'find-in-ssd
  )
@@ -51,6 +56,19 @@
  :keymap 'python-mode-map
  "C-;" 'add-common-symbol
  )
+
+;; Dunp-Jump
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+(setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+(defhydra dumb-jump-hydra (:color blue :columns 3)
+    "Dumb Jump"
+    ("j" dumb-jump-go "Go")
+    ("o" dumb-jump-go-other-window "Other window")
+    ("e" dumb-jump-go-prefer-external "Go external")
+    ("x" dumb-jump-go-prefer-external-other-window "Go external other window")
+    ("i" dumb-jump-go-prompt "Prompt")
+    ("l" dumb-jump-quick-look "Quick look")
+    ("b" dumb-jump-back "Back"))
 
 (global-set-key [next] 'good-scroll-up-full-screen)
 (global-set-key [prior] 'good-scroll-down-full-screen)
@@ -126,4 +144,21 @@
 (defun find-in-ssd ()
   (interactive)
   (find-file "/Volumes/Samsung_T5/" ))
+
+(defvar my-window-height (/ (* 2 (window-height)) 3))
+(defun my-scroll-up ()
+  (interactive)
+  (scroll-up my-window-height))
+
+(defun my-scroll-down ()
+  (interactive)
+  (scroll-down my-window-height))
+
+(defun my-scroll-other-window-up ()
+  (interactive)
+  (scroll-other-window-up my-window-height))
+
+(defun my-scroll-other-window-down ()
+  (interactive)
+  (scroll-other-window-down my-window-height))
 

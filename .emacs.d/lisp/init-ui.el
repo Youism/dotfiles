633e10e6eb57bbccb9e8ae236eae2520577a55dd
peng-yo <2485166642@qq.com>
2023-05-05 18:45:22 +0800

    enable whitespace mode and set it globally.

--- .emacs.d/lisp/init-ui.el
+++ .emacs.d/lisp/init-ui.el
@@ -113,4 +113,46 @@
 (add-hook 'org-mode-hook
 	  (lambda()
 	    (setq truncate-lines nil)))
+;; Write spacemode
+;; Define the whitespace style.
+(setq-default whitespace-style
+              '(face spaces empty tabs newline trailing space-mark tab-mark newline-mark))
+
+;; Whitespace color corrections.
+(require 'color)
+(let* ((ws-lighten 30) ;; Amount in percentage to lighten up black.
+       (ws-color (color-lighten-name "#000000" ws-lighten)))
+  (custom-set-faces
+   `(whitespace-newline                ((t (:foreground ,ws-color))))
+   `(whitespace-missing-newline-at-eof ((t (:foreground ,ws-color))))
+   `(whitespace-space                  ((t (:foreground ,ws-color))))
+   `(whitespace-space-after-tab        ((t (:foreground ,ws-color))))
+   `(whitespace-space-before-tab       ((t (:foreground ,ws-color))))
+   `(whitespace-tab                    ((t (:foreground ,ws-color))))
+   `(whitespace-trailing               ((t (:foreground ,ws-color))))))
+
+;; Make these characters represent whitespace.
+(setq-default whitespace-display-mappings
+	      '(
+		;; space -> · else .
+		(space-mark 32 [183] [46])
+		;; tabs -> » else >
+		(tab-mark ?\t [187 ?\t] [62 ?\t])))
+
+(global-whitespace-mode 1)
+;; Don't enable whitespace for.
+(setq-default whitespace-global-modes
+              '(not shell-mode
+                    help-mode
+                    magit-mode
+                    magit-diff-mode
+                    ibuffer-mode
+                    dired-mode
+                    occur-mode))
+
+;; Set whitespace actions.
+(setq-default whitespace-action
+              '(cleanup auto-cleanup))
+
+
 (provide 'init-ui)
